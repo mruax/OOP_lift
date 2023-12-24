e32387fa88834486051b576fb11878714d996ba0
mruax <matvey.chernyakov@yandex.ru>
2023-12-24 19:11:09 +0500

    added qwidgets show

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from PyQt5.QtGui import QIcon
 from qasync import QEventLoop, asyncSlot
 from pathlib import Path
 from random import randint, choice
+from queue import Queue
 
 from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QLabel, QPushButton
 import sys
@@ -13,7 +14,7 @@ from generated_3floor_lift import Ui_Form
 
 
 class MainWindow(QMainWindow):
-    def __init__(self, loop=None):
+    def __init__(self, loop, elevators, controller, elevator_views):
         super().__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
@@ -24,22 +25,42 @@ class MainWindow(QMainWindow):
 
         self.loop = loop
 
-        self.ui.st1_home1_lift1.clicked.connect(self.open_lift_window)
+        self.elevators = elevators
+        self.controller = controller
+        self.elevator_views = elevator_views
+
+        self.lift_window = None
+        # self.ui.lift_1.clicked.connect(self.open_lift_window(1))
+        for i in range(1, 65):
+            lift_button = getattr(self.ui, f"lift_{i}")
+            lift_button.clicked.connect(lambda _, id=i: self.open_lift_window(id))
 
     def closeEvent(self, event):
         self.loop.exec()  # по сути raise error xdd
         event.accept()  # Принимаем событие закрытия
 
-    def open_lift_window(self):
-        self.lift_window = ElevatorView()
-        self.lift_window.show()
+    # @asyncSlot()
+    def open_lift_window(self, id):
+        self.elevator_views[id - 1].show()
 
 
 class Elevator:
-    def __init__(self, elevator_id):
+    def __init__(self, street_id, house_id, elevator_id, capacity):
+        # Расположение
+        self.street_id = street_id
+        self.house_id = house_id
         self.elevator_id = elevator_id
+
+        # Технические хар-ки
+        self.capacity = capacity  # грузоподьёмность
+        self.passengers = 0  # количество пассажиров внутри
+        self.door_status = False  # двери открыты/закрыты
+        self.lift_status = True  # состояние лифта вкл/выкл
+
+        # Переменные логики
         self.current_floor = 1
         self.target_floor = None
+        self.floors_queue = Queue()
         self.callbacks = []
 
     def move_to_floor(self, target_floor):
@@ -63,13 +84,13 @@ class ElevatorController:
 
 
 class ElevatorView(QWidget):
-    def __init__(self, controller=1 , elevator=1):
+    def __init__(self, controller, elevator):
         super().__init__()
         self.ui = Ui_Form()
         self.ui.setupUi(self)
         self.controller = controller
         self.elevator = elevator
-        # self.setWindowTitle(f"Elevator {elevator.elevator_id}")
+        self.setWindowTitle(f"Elevator {elevator.elevator_id}")
         # self.setGeometry(100, 100, 300, 200)
 
         # self.call_button = QPushButton("Call Elevator", self)
@@ -97,20 +118,24 @@ def main():
     app = QApplication(sys.argv)
     loop = QEventLoop(app)
     asyncio.set_event_loop(loop)
-    window = MainWindow(loop)
+
+    num_elevators = 4 * 4 * 4
+    elevators = []
+    for id in range(1, num_elevators + 1):
+        elevators.append(Elevator(street_id=(id + 4 - 1) // 2 + 1, house_id=id % 4 + 1, elevator_id=id,
+                                  capacity=randint(6, 14) * 50))
+    controller = ElevatorController(elevators)
+    elevator_views = [ElevatorView(controller, elevator) for elevator in elevators]
+
+    # for view in elevator_views:
+    #     view.show()
+
+    # Отобразим главное окно после создания лифтов
+    window = MainWindow(loop=loop, elevators=elevators, controller=controller, elevator_views=elevator_views)
     window.show()
 
-    # num_elevators = 3
-    # elevators = [Elevator(elevator_id=i) for i in range(num_elevators)]
-    # controller = ElevatorController(elevators)
-    #
-    # elevator_views = [ElevatorView(controller, elevator) for elevator in elevators]
-    #
-    # # for view in elevator_views:
-    # #     view.show()
-    #
-    # with loop:
-    #     loop.run_forever()
+    with loop:
+        loop.run_forever()
 
     sys.exit(app.exec())
 
